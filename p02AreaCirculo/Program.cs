4b72dd88cdf668d03cd215a6887cbcc210ed868a
alberto09ac35 <alberto09ac35@gmail.com>
2020-08-27 13:47:04 -0500

    Actualixado

---
+++ p02AreaCirculo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace p02AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float  radio = 0, area =0;
+
+            Console.Clear();
+            Console.WriteLine("Dame el radio del circulo");
+            radio = float.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(radio,2);
+
+            Console.WriteLine($"El area es{area}");
+        }
+    }
+}
